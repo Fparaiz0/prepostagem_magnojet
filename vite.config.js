5d30ce9ba68e93f8e4c1ea01e03db2c64766721e
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-13 17:15:12 -0300

    adds lint:check and lint:fix scripts and prettier code formatting

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     laravel({
-      input: ['resources/js/app.js'], 
+      input: ["resources/js/app.js"],
       refresh: true,
     }),
   ],
   build: {
     manifest: true,
-    outDir: 'public/build',
+    outDir: "public/build",
   },
   server: {
-        host: '10.1.1.50',
-        port: 8001, 
-        strictPort: true,
-        hmr: {
-            host: '10.1.1.50', 
-        }
-    }
+    host: "10.1.1.50",
+    port: 8001,
+    strictPort: true,
+    hmr: {
+      host: "10.1.1.50",
+    },
+  },
 });
