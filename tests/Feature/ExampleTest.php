2033f4baa423fda7922166ded702e028b76bdba0
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-27 08:44:50 -0300

    starting automated testing for security and performance

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,13 +7,10 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+  public function test_the_application_returns_a_successful_response(): void
+  {
+    $response = $this->get('http://127.0.0.1:8000/api/remetentes/buscar');
 
-        $response->assertStatus(200);
-    }
+    $response->assertStatus(200);
+  }
 }
