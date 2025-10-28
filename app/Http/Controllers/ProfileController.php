9b6ff0672db6ca9133687c4096df8998f641487b
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 14:09:19 -0300

    remove all comments from the `ProfileController.php` file

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,125 +10,96 @@ use Illuminate\Support\Facades\Log;
 
 class ProfileController extends Controller
 {
-    // Visualizar os detalhes do perfil
-    public function show()
-    {
-        // Recuperar do banco de dados as informações do usuário logado
-        $user = User::where('id', Auth::id())->first();
+  public function show()
+  {
+    $user = User::where('id', Auth::id())->first();
 
-        // Salvar log
-        Log::info('Visualizar o perfil.', ['action_user_id' => Auth::id()]);
+    Log::info('Visualizar o perfil.', ['action_user_id' => Auth::id()]);
 
-        // Carregar a view
-        return view('profile.show', ['user' => $user]);
+    return view('profile.show', ['user' => $user]);
+  }
+
+  public function edit()
+  {
+    $user = User::where('id', Auth::id())->first();
+
+    Log::info('Formulario editar o perfil.', ['action_user_id' => Auth::id()]);
+
+    return view('profile.edit', ['user' => $user]);
+  }
+
+  public function update(Request $request)
+  {
+    $user = User::where('id', Auth::id())->first();
+
+    $request->validate(
+      [
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email,' . ($user ? $user->id : null),
+      ],
+      [
+        'name.required' => 'Campo nome é obrigatório!',
+        'email.required' => 'Campo e-mail é obrigatório!',
+        'email.email' => 'Necessário enviar e-mail válido!',
+        'email.unique' => 'O e-mail já está cadastrado!',
+      ]
+    );
+
+    try {
+
+      $user->update([
+        'name' => $request->name,
+        'email' => $request->email,
+      ]);
+
+      Log::info('Perfil editado.', ['action_user_id' => Auth::id()]);
+
+      return redirect()->route('profile.show', ['user' => $user->id])->with('success', 'Perfil editado com sucesso!');
+    } catch (Exception $e) {
+
+      Log::notice('Perfil não editado.', ['error' => $e->getMessage(), 'action_user_id' => Auth::id()]);
+
+      return back()->withInput()->with('error', 'Perfil não editado!');
     }
+  }
 
-    // Carregar o formulário editar perfil
-    public function edit()
-    {
-        // Recuperar do banco de dados as informações do usuário logado
-        $user = User::where('id', Auth::id())->first();
+  public function editPassword()
+  {
+    $user = User::where('id', Auth::id())->first();
 
-        // Salvar log
-        Log::info('Formulario editar o perfil.', ['action_user_id' => Auth::id()]);
+    return view('profile.edit_password', ['user' => $user]);
+  }
 
-        // Carregar a view
-        return view('profile.edit', ['user' => $user]);
+  public function updatePassword(Request $request)
+  {
+
+    $request->validate(
+      [
+        'password' => 'required|confirmed|min:6',
+      ],
+      [
+        'password.required' => 'Campo senha é obrigatório!',
+        'password.confirmed' => 'A confirmação da senha não corresponde!',
+        'password.min' => 'Senha com no mínimo :min caracteres!',
+      ]
+    );
+
+    try {
+
+      $user = User::where('id', Auth::id())->first();
+
+      $user->update([
+        'password' => $request->password,
+      ]);
+
+      Log::info('Senha do perfil editado.', ['action_user_id' => Auth::id()]);
+
+      return redirect()->route('profile.show')->with('success', 'Senha do perfil editada com sucesso!');
+    } catch (Exception $e) {
+
+      Log::notice('Senha do perfil não editada.', ['error' => $e->getMessage(), 'action_user_id' => Auth::id()]);
+
+      return back()->withInput()->with('error', 'Senha do perfil não editada!');
     }
-
-    // Editar no banco de dados o perfil
-    public function update(Request $request)
-    {
-        // Recuperar do banco de dados as informações do usuário logado
-        $user = User::where('id', Auth::id())->first();
-
-        // Validar o formulário
-        $request->validate(
-            [
-                'name' => 'required',
-                'email' => 'required|email|unique:users,email,'.($user ? $user->id : null),
-            ],
-            [
-                'name.required' => 'Campo nome é obrigatório!',
-                'email.required' => 'Campo e-mail é obrigatório!',
-                'email.email' => 'Necessário enviar e-mail válido!',
-                'email.unique' => 'O e-mail já está cadastrado!',
-            ]
-        );
-
-        // Capturar possíveis exceções durante a execução.
-        try {
-
-            // Editar as informações do registro no banco de dados
-            $user->update([
-                'name' => $request->name,
-                'email' => $request->email,
-            ]);
-
-            // Salvar log
-            Log::info('Perfil editado.', ['action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso
-            return redirect()->route('profile.show', ['user' => $user->id])->with('success', 'Perfil editado com sucesso!');
-        } catch (Exception $e) {
-
-            // Salvar log
-            Log::notice('Perfil não editado.', ['error' => $e->getMessage(), 'action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Perfil não editado!');
-        }
-    }
-
-    // Carregar o formulário editar senha do perfil
-    public function editPassword()
-    {
-        // Recuperar do banco de dados as informações do usuário logado
-        $user = User::where('id', Auth::id())->first();
-
-        // Carregar a view
-        return view('profile.edit_password', ['user' => $user]);
-    }
-
-    // Editar no banco de dados a senha do perfil
-    public function updatePassword(Request $request)
-    {
-
-        // Validar o formulário
-        $request->validate(
-            [
-                'password' => 'required|confirmed|min:6',
-            ],
-            [
-                'password.required' => 'Campo senha é obrigatório!',
-                'password.confirmed' => 'A confirmação da senha não corresponde!',
-                'password.min' => 'Senha com no mínimo :min caracteres!',
-            ]
-        );
-
-        // Capturar possíveis exceções durante a execução.
-        try {
-
-            // Recuperar do banco de dados as informações do usuário logado
-            $user = User::where('id', Auth::id())->first();
-
-            // Editar as informações do registro no banco de dados
-            $user->update([
-                'password' => $request->password,
-            ]);
-
-            // Salvar log
-            Log::info('Senha do perfil editado.', ['action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso
-            return redirect()->route('profile.show')->with('success', 'Senha do perfil editada com sucesso!');
-        } catch (Exception $e) {
-
-            // Salvar log
-            Log::notice('Senha do perfil não editada.', ['error' => $e->getMessage(), 'action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Senha do perfil não editada!');
-        }
-    }
+  }
 }
