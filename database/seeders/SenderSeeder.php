a27edff8aee21944adfc38e79ec1974198c8399c
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 15:11:45 -0300

    remove all comments from the `SenderSeeder.php` file

--- database/seeders/SenderSeeder.php
+++ database/seeders/SenderSeeder.php
@@ -9,33 +9,28 @@ use Illuminate\Support\Facades\Log;
 
 class SenderSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //Capturar possíveis exceções durante a execução do código.
-        try {
-            // Se não encontrar o registro com o nome, cadastra o registro no BD
-            Sender::firstOrCreate(
-                [
-                'name' => 'MAGNO JET INDUSTRIA LTDA', 
-                'id' => 1
-                ],
-                [
-                'id' => 1, 
-                'name' => 'MAGNO JET INDUSTRIA LTDA',
-                'cnpj' => '06092428000198', 
-                'cep' => '84900000',
-                'public_place' => 'Avenida Governador Paulo Cruz Pimentel',
-                'number' => '1051', 
-                'neighborhood' => 'Centro', 
-                'city' => 'Ibaiti', 
-                'uf' => 'PR'
-                ],
-            );
-        }catch (Exception $e) {
-            Log::notice('Remetente não cadastrado.', ['error' => $e->getMessage()]);
-        }
+  public function run(): void
+  {
+    try {
+      Sender::firstOrCreate(
+        [
+          'name' => 'MAGNO JET INDUSTRIA LTDA',
+          'id' => 1
+        ],
+        [
+          'id' => 1,
+          'name' => 'MAGNO JET INDUSTRIA LTDA',
+          'cnpj' => '06092428000198',
+          'cep' => '84900000',
+          'public_place' => 'Avenida Governador Paulo Cruz Pimentel',
+          'number' => '1051',
+          'neighborhood' => 'Centro',
+          'city' => 'Ibaiti',
+          'uf' => 'PR'
+        ],
+      );
+    } catch (Exception $e) {
+      Log::notice('Remetente não cadastrado.', ['error' => $e->getMessage()]);
     }
+  }
 }
