f16288aa53476a38a7d8e8e358f41cf83c1ef5b5
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 14:19:37 -0300

    remove all comments from the `/Feature/ExampleTest.php` file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-  /**
-   * A basic test example.
-   */
   public function test_the_application_returns_a_successful_response(): void
   {
-    $response = $this->get('http://10.1.1.50:8000/api/embalagens/buscar');
+    $response = $this->get('http://10.0.0.50:8000/api/remetentes/buscar');
 
     $response->assertStatus(200);
   }
