fe3139c410c4f6bb30e6765602909e172972b3eb
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 14:45:27 -0300

    remove all comments from the `Recipient.php` file

--- app/Models/Recipient.php
+++ app/Models/Recipient.php
@@ -7,22 +7,20 @@ use OwenIt\Auditing\Contracts\Auditable;
 
 class Recipient extends Model implements Auditable
 {
-    use \OwenIt\Auditing\Auditable;
+  use \OwenIt\Auditing\Auditable;
 
-    // Indicar o nome da tabela
-    protected $table = 'recipients';
+  protected $table = 'recipients';
 
-    // Indicar quais colunas podem ser manipuladas
-    protected $fillable = [
-        'recipient_id',
-        'name',
-        'cnpj',
-        'cep',
-        'public_place',
-        'number',
-        'complement',
-        'neighborhood',
-        'city',
-        'uf',
-    ];
+  protected $fillable = [
+    'recipient_id',
+    'name',
+    'cnpj',
+    'cep',
+    'public_place',
+    'number',
+    'complement',
+    'neighborhood',
+    'city',
+    'uf',
+  ];
 }
