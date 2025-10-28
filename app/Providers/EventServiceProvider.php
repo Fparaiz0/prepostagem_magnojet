9c11d90048090250cf043512ce54534f6259ada1
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 14:51:53 -0300

    remove all comments from the `EventServiceProvider.php` file

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,25 +6,15 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * O mapeamento dos eventos para os listeners da aplicação.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        // Evento de login do Laravel
-        \Illuminate\Auth\Events\Login::class => [
-            \App\Listeners\AtualizaTokenCorreiosApósLogin::class,
-        ],
-    ];
+  protected $listen = [
+    // Evento de login do Laravel
+    \Illuminate\Auth\Events\Login::class => [
+      \App\Listeners\AtualizaTokenCorreiosApósLogin::class,
+    ],
+  ];
 
-    /**
-     * Registra quaisquer eventos para sua aplicação.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  public function boot()
+  {
+    //
+  }
 }
