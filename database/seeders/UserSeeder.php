5d4c8ef699260170dec31d9730627c79ddd1097d
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 15:12:43 -0300

    remove all comments from the `UserSeeder.php` file

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,50 +10,39 @@ use Illuminate\Support\Facades\Log;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
+  public function run(): void
+  {
 
-        // Capturar possíveis exceções durante a execução do seeder. 
-        try {
-            // Verificar se o usuário está cadastrado no banco de dados
-            if (!User::where('email', 'fparaizo3@gmail.com')->first()) {
-                // Cadastrar o usuário
-                $superAdmin = User::create([
-                    'name' => 'Felipe Paraizo',
-                    'email' => 'fparaizo3@gmail.com',
-                    'password' => '123456A#',
-                ]);
+    try {
+      if (!User::where('email', 'fparaizo3@gmail.com')->first()) {
+        $superAdmin = User::create([
+          'name' => 'Felipe Paraizo',
+          'email' => 'fparaizo3@gmail.com',
+          'password' => '123456A#',
+        ]);
 
-                // Atribuir papel para o usuário
-                $superAdmin->assignRole('Super Admin');
+        $superAdmin->assignRole('Super Admin');
 
-            }
+      }
 
-            if (App::environment() !== 'production') {
-                // Se não encontrar o registro com o e-mail, cadastra o registro no BD
-                $admin = User::firstOrCreate(
-                    ['email' => 'aldaircunha@terra.com.br'],
-                    ['name' => 'Aldair', 'email' => 'aldaircunha@terra.com.br', 'password' => '123456A#'],
-                );
+      if (App::environment() !== 'production') {
+        $admin = User::firstOrCreate(
+          ['email' => 'aldaircunha@terra.com.br'],
+          ['name' => 'Aldair', 'email' => 'aldaircunha@terra.com.br', 'password' => '123456A#'],
+        );
 
-                // Atribuir papel para o usuário
-                $admin->assignRole('Admin');
+        $admin->assignRole('Admin');
 
-                // Se não encontrar o registro com o e-mail, cadastra o registro no BD
-                $colaborador = User::firstOrCreate(
-                    ['email' => 'tanio@magnojet.com.br'],
-                    ['name' => 'Tanio', 'email' => 'tanio@magnojet.com.br', 'password' => '123456A#'],
-                );
+        $colaborador = User::firstOrCreate(
+          ['email' => 'tanio@magnojet.com.br'],
+          ['name' => 'Tanio', 'email' => 'tanio@magnojet.com.br', 'password' => '123456A#'],
+        );
 
-                // Atribuir papel para o usuário
-                $colaborador->assignRole('Colaborador');
-            }
+        $colaborador->assignRole('Colaborador');
+      }
 
-        } catch (Exception $e) {
-            Log::notice('Usuário não cadastrado.', ['error' => $e->getMessage()]);
-        }
+    } catch (Exception $e) {
+      Log::notice('Usuário não cadastrado.', ['error' => $e->getMessage()]);
     }
+  }
 }
