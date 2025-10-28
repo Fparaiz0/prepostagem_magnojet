be236127d54f92d3b1d3ff6bc22eb7e0d4ed0702
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 14:35:43 -0300

    remove all comments from the `AtualizaTokenCorreios.php` file

--- app/Listeners/AtualizaTokenCorreiosApósLogin.php
+++ app/Listeners/AtualizaTokenCorreiosApósLogin.php
@@ -7,21 +7,15 @@ use Illuminate\Auth\Events\Login;
 
 class AtualizaTokenCorreiosApósLogin
 {
-    protected CorreiosTokenService $service;
+  protected CorreiosTokenService $service;
 
-    public function __construct(CorreiosTokenService $service)
-    {
-        $this->service = $service;
-    }
+  public function __construct(CorreiosTokenService $service)
+  {
+    $this->service = $service;
+  }
 
-    /**
-     * Handle the event.
-     *
-     * @return void
-     */
-    public function handle(Login $event)
-    {
-        // Aqui roda a lógica para atualizar o token automaticamente
-        $this->service->obterToken();
-    }
+  public function handle(Login $event)
+  {
+    $this->service->obterToken();
+  }
 }
