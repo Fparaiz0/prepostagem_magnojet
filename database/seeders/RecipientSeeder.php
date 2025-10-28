4cb165e417827d2c0de12b2f365ea89c446d6b21
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 15:09:14 -0300

    remove all comments from the `RecipientSeeder.php` file

--- database/seeders/RecipientSeeder.php
+++ database/seeders/RecipientSeeder.php
@@ -9,33 +9,28 @@ use Illuminate\Support\Facades\Log;
 
 class RecipientSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //Capturar possíveis exceções durante a execução do código.
-        try {
-            // Se não encontrar o registro com o nome, cadastra o registro no BD
-            Recipient::firstOrCreate(
-                [
-                'name' => 'LMC PULVERIZADORES', 
-                'id' => 1
-                ],
-                [
-                'id' => 1, 
-                'name' => 'LMC PULVERIZADORES',
-                'cnpj' => '12345678000195', 
-                'cep' => '84900000',
-                'public_place' => 'BR-153, KM 103',
-                'number' => 'S/N', 
-                'neighborhood' => 'BR', 
-                'city' => 'Ibaiti', 
-                'uf' => 'PR'
-                ],
-            );
-        }catch (Exception $e) {
-            Log::notice('Destinatário não cadastrado.', ['error' => $e->getMessage()]);
-        }
+  public function run(): void
+  {
+    try {
+      Recipient::firstOrCreate(
+        [
+          'name' => 'LMC PULVERIZADORES',
+          'id' => 1
+        ],
+        [
+          'id' => 1,
+          'name' => 'LMC PULVERIZADORES',
+          'cnpj' => '12345678000195',
+          'cep' => '84900000',
+          'public_place' => 'BR-153, KM 103',
+          'number' => 'S/N',
+          'neighborhood' => 'BR',
+          'city' => 'Ibaiti',
+          'uf' => 'PR'
+        ],
+      );
+    } catch (Exception $e) {
+      Log::notice('Destinatário não cadastrado.', ['error' => $e->getMessage()]);
     }
+  }
 }
