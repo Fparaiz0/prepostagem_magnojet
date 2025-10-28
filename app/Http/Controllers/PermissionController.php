09475726b5c913b2eeb19800239b2de45f1f99fc
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 13:36:55 -0300

    remove all comments from the `PermissionController.php` file

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -10,116 +10,84 @@ use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    // Listar as permissões ou páginas
-    public function index()
-    {
-        // Recuperar os registros do banco dados
-        $permissions = Permission::orderBy('title', 'ASC')->paginate(50);
+  public function index()
+  {
+    $permissions = Permission::orderBy('title', 'ASC')->paginate(50);
 
-        // Salvar log
-        Log::info('Listar as permissões.', ['action_user_id' => Auth::id()]);
+    Log::info('Listar as permissões.', ['action_user_id' => Auth::id()]);
 
-        // Carregar a view
-        return view('permissions.index', ['permissions' => $permissions]);
+    return view('permissions.index', ['permissions' => $permissions]);
+  }
+
+  public function show(Permission $permission)
+  {
+    Log::info('Visualizar a permissão.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
+
+    return view('permissions.show', ['permission' => $permission]);
+  }
+
+  public function create()
+  {
+    return view('permissions.create');
+  }
+
+  public function store(PermissionRequest $request)
+  {
+    try {
+      $permission = Permission::create([
+        'title' => $request->title,
+        'name' => $request->name,
+      ]);
+
+      Log::info('Permissão cadastrada.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
+
+      return redirect()->route('permissions.show', ['permission' => $permission->id])->with('success', 'Permissão cadastrada com sucesso!');
+    } catch (Exception $e) {
+
+      Log::notice('Permissão não cadastrada.', ['error' => $e->getMessage(), 'action_user_id' => Auth::id()]);
+
+      return back()->withInput()->with('error', 'Permissão não cadastrada!');
     }
+  }
 
-    // Visualizar os detalhes da permissão ou página
-    public function show(Permission $permission)
-    {
-        // Salvar log
-        Log::info('Visualizar a permissão.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
+  public function edit(Permission $permission)
+  {
+    return view('permissions.edit', ['permission' => $permission]);
+  }
 
-        // Carregar a view
-        return view('permissions.show', ['permission' => $permission]);
+  public function update(PermissionRequest $request, Permission $permission)
+  {
+    try {
+      $permission->update([
+        'title' => $request->title,
+        'name' => $request->name,
+      ]);
+
+      Log::info('Permissão editada.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
+
+      return redirect()->route('permissions.show', ['permission' => $permission->id])->with('success', 'Permissão editada com sucesso!');
+    } catch (Exception $e) {
+
+      Log::notice('Permissão não editada.', ['error' => $e->getMessage()]);
+
+      return back()->withInput()->with('error', 'Permissão não editada!');
     }
+  }
 
-    // Carregar o formulário cadastrar nova permissão ou página
-    public function create()
-    {
-        // Carregar a view
-        return view('permissions.create');
+  public function destroy(Permission $permission)
+  {
+    try {
+
+      $permission->delete();
+
+      Log::info('Permissão apagada.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
+
+      return redirect()->route('permissions.index')->with('success', 'Permissão apagada com sucesso!');
+    } catch (Exception $e) {
+
+      Log::notice('Permissão não apagada.', ['error' => $e->getMessage()]);
+
+      return back()->withInput()->with('error', 'Permissão não apagada!');
     }
-
-    // Cadastrar no banco de dados o nova permissão ou página
-    public function store(PermissionRequest $request)
-    {
-        // Capturar possíveis exceções durante a execução.
-        try {
-            // Cadastrar no banco de dados na tabela permissão
-            $permission = Permission::create([
-                'title' => $request->title,
-                'name' => $request->name,
-            ]);
-
-            // Salvar log
-            Log::info('Permissão cadastrada.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso
-            return redirect()->route('permissions.show', ['permission' => $permission->id])->with('success', 'Permissão cadastrada com sucesso!');
-        } catch (Exception $e) {
-
-            // Salvar log
-            Log::notice('Permissão não cadastrada.', ['error' => $e->getMessage(), 'action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Permissão não cadastrada!');
-        }
-    }
-
-    // Carregar o formulário editar permissão ou página
-    public function edit(Permission $permission)
-    {
-        // Carregar a view
-        return view('permissions.edit', ['permission' => $permission]);
-    }
-
-    // Editar no banco de dados a permissão ou página
-    public function update(PermissionRequest $request, Permission $permission)
-    {
-        // Capturar possíveis exceções durante a execução.
-        try {
-            // Editar as informações do registro no banco de dados
-            $permission->update([
-                'title' => $request->title,
-                'name' => $request->name,
-            ]);
-
-            // Salvar log
-            Log::info('Permissão editada.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso
-            return redirect()->route('permissions.show', ['permission' => $permission->id])->with('success', 'Permissão editada com sucesso!');
-        } catch (Exception $e) {
-
-            // Salvar log
-            Log::notice('Permissão não editada.', ['error' => $e->getMessage()]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Permissão não editada!');
-        }
-    }
-
-    // Excluir a permissão ou página do banco de dados
-    public function destroy(Permission $permission)
-    {
-        // Capturar possíveis exceções durante a execução.
-        try {
-
-            // Excluir o registro do banco de dados
-            $permission->delete();
-
-            // Salvar log
-            Log::info('Permissão apagada.', ['permission_id' => $permission->id, 'action_user_id' => Auth::id()]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso
-            return redirect()->route('permissions.index')->with('success', 'Permissão apagada com sucesso!');
-        } catch (Exception $e) {
-
-            // Salvar log
-            Log::notice('Permissão não apagada.', ['error' => $e->getMessage()]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Permissão não apagada!');
-        }
-    }
+  }
 }
