42a02633ed55d6db5ea770a0ba0571201d0b6214
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-28 13:28:05 -0300

    remove all comments from the `ForgotPasswordController.php` file

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -10,136 +10,104 @@ use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
-    // Formulário para receber o link recuperar senha
-    public function showLinkRequestForm()
-    {
-        // Carregar a VIEW
-        return view('auth.forgot_password');
+  public function showLinkRequestForm()
+  {
+    return view('auth.forgot_password');
+  }
+
+  public function sendResetLinkEmail(Request $request)
+  {
+    $request->validate(
+      [
+        'email' => 'required|email',
+      ],
+      [
+        'email.required' => 'Campo e-mail é obrigatório',
+        'email.email' => 'Necessario enviar e-mail válido',
+      ]
+    );
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user) {
+      Log::notice('Tentativa de recuperação de senha com e-mail não cadastrado', ['email' => $request->email]);
+
+      return back()->withInput()->with('error', 'E-mail não encontrado!');
     }
 
-    // Receber os dados do formulário recuperar senha
-    public function sendResetLinkEmail(Request $request)
-    {
-        // Validar o formulário
-        $request->validate(
-            [
-                'email' => 'required|email',
-            ],
-            [
-                'email.required' => 'Campo e-mail é obrigatório',
-                'email.email' => 'Necessario enviar e-mail válido',
-            ]
-        );
+    try {
+      $status = Password::sendResetLink(
+        $request->only('email')
+      );
 
-        // Verificar se existe usuário no banco de dados com o e-mail
-        $user = User::where('email', $request->email)->first();
+      Log::info('Recuperar senha.', ['status' => $status, 'email' => $request->email]);
 
-        // Verificar se encontrou o usuário
-        if (! $user) {
-            // Salvar log
-            Log::notice('Tentativa de recuperação de senha com e-mail não cadastrado', ['email' => $request->email]);
+      return redirect()->route('login')->with('success', 'Enviado e-mail com instruções para recuperar a senha. Acesse a sua caixa de e-mail para recuperar a senha!');
+    } catch (Exception $e) {
+      Log::notice('Erro recuperar senha.', ['error' => $e->getMessage(), 'email' => $request->email]);
 
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'E-mail não encontrado!');
-        }
-
-        try {
-            // Salvar o token recuperar senha e enviar o e-mail
-            $status = Password::sendResetLink(
-                // Retorna um array associativo contendo apenas o campo "email" da requisição.
-                $request->only('email')
-            );
-
-            // Salvar log
-            Log::info('Recuperar senha.', ['status' => $status, 'email' => $request->email]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso
-            return redirect()->route('login')->with('success', 'Enviado e-mail com instruções para recuperar a senha. Acesse a sua caixa de e-mail para recuperar a senha!');
-        } catch (Exception $e) {
-            // Salvar log
-            Log::notice('Erro recuperar senha.', ['error' => $e->getMessage(), 'email' => $request->email]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Tente mais tarde!');
-        }
+      return back()->withInput()->with('error', 'Tente mais tarde!');
     }
+  }
 
-    // Carregar o formulário atualizar senha
-    public function showRequestForm(Request $request)
-    {
-        try {
-            // Recuperar os dados do usuário no banco de dados através do e-mail
-            $user = User::where('email', $request->email)->first();
+  public function showRequestForm(Request $request)
+  {
+    try {
+      $user = User::where('email', $request->email)->first();
 
-            // Verificar se encontrou o usuário no BD e o token é válido
-            if (! $user || ! Password::tokenExists($user, $request->token)) {
-                // Salvar log
-                Log::notice('Token inválido ou expirado.', ['email' => $request->email ? $request->email : '']);
+      if (!$user || !Password::tokenExists($user, $request->token)) {
+        Log::notice('Token inválido ou expirado.', ['email' => $request->email ? $request->email : '']);
 
-                // Redirecionar o usuário, enviar a mensagem de erro
-                return redirect()->route('login')->with('error', 'Token inválido ou expirado!');
-            }
+        return redirect()->route('login')->with('error', 'Token inválido ou expirado!');
+      }
 
-            // Carregar a VIEW
-            return view('auth.reset_password', ['token' => $request->token, 'email' => $request->email]);
+      return view('auth.reset_password', ['token' => $request->token, 'email' => $request->email]);
 
-        } catch (Exception $e) {
-            // Salvar log
-            Log::notice('Token inválido ou expirado.', ['error' => $e->getMessage(), 'email' => $request->email]);
+    } catch (Exception $e) {
+      Log::notice('Token inválido ou expirado.', ['error' => $e->getMessage(), 'email' => $request->email]);
 
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return redirect()->route('login')->with('error', 'Token inválido ou expirado!');
-        }
+      return redirect()->route('login')->with('error', 'Token inválido ou expirado!');
     }
+  }
 
-    public function reset(Request $request)
-    {
-        // Validar o formulário
-        $request->validate(
-            [
-                'email' => 'required|email|exists:users',
-                'password' => 'required|confirmed|min:6',
-            ],
-            [
-                'email.required' => 'Campo e-mail é obrigatório',
-                'email.email' => 'Necessario enviar e-mail válido',
-                'email.exists' => 'E-mail inválido, utilize o e-mail cadastrado!',
-                'password.required' => 'Campo senha é obrigatório!',
-                'password.confirmed' => 'A confirmação da senha não corresponde',
-                'password.min' => 'Senha com no mínimo :min caracteres!',
-            ]
-        );
+  public function reset(Request $request)
+  {
+    $request->validate(
+      [
+        'email' => 'required|email|exists:users',
+        'password' => 'required|confirmed|min:6',
+      ],
+      [
+        'email.required' => 'Campo e-mail é obrigatório',
+        'email.email' => 'Necessario enviar e-mail válido',
+        'email.exists' => 'E-mail inválido, utilize o e-mail cadastrado!',
+        'password.required' => 'Campo senha é obrigatório!',
+        'password.confirmed' => 'A confirmação da senha não corresponde',
+        'password.min' => 'Senha com no mínimo :min caracteres!',
+      ]
+    );
 
-        try {
-            // reset - Redefinir a senha do usuário
-            $status = Password::reset(
-                // only - Recuperar apenas os campos específicos do pedido: 'email', 'password', 'password_confirmation' e 'token'.
-                $request->only('email', 'password', 'password_confirmation', 'token'),
+    try {
+      $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
 
-                // Retornar o callback se a redefinição de senha for bem-sucedida
-                function (User $user, string $password) {
-                    // forceFill - Forçar o acesso a atributos protegidos
-                    $user->forceFill([
-                        'password' => $password,
-                    ]);
+        function (User $user, string $password) {
+          $user->forceFill([
+            'password' => $password,
+          ]);
 
-                    // Salvar as alterações
-                    $user->save();
-                }
-            );
-
-            // Salvar log
-            Log::info('Senha atualizada.', ['status' => $status, 'email' => $request->email]);
-
-            // Redirecionar o usuário, enviar a mensagem de sucesso ou erro
-            return $status === Password::PASSWORD_RESET ? redirect()->route('login')->with('success', 'Senha atualizada com sucesso!') : redirect()->route('login')->with('error', 'Senha não atualizada!');
-
-        } catch (Exception $e) {
-            // Salvar log
-            Log::notice('Erro atualizar senha.', ['error' => $e->getMessage(), 'email' => $request->email]);
-
-            // Redirecionar o usuário, enviar a mensagem de erro
-            return back()->withInput()->with('error', 'Tente mais tarde!');
+          $user->save();
         }
+      );
+
+      Log::info('Senha atualizada.', ['status' => $status, 'email' => $request->email]);
+
+      return $status === Password::PASSWORD_RESET ? redirect()->route('login')->with('success', 'Senha atualizada com sucesso!') : redirect()->route('login')->with('error', 'Senha não atualizada!');
+
+    } catch (Exception $e) {
+      Log::notice('Erro atualizar senha.', ['error' => $e->getMessage(), 'email' => $request->email]);
+
+      return back()->withInput()->with('error', 'Tente mais tarde!');
     }
+  }
 }
