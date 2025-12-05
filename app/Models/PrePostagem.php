2dd1bc5b0d32290f3f10a7fd8e635e7f6af2c825
Felipe Paraizo <fparaizo3@gmail.com>
2025-12-05 13:23:04 -0300

    refactor: apply correct PascalCase to PrePostagem model for Linux compatibility
    Updates the Prepostagem model class name and file path to PrePostagem.php, adhering to PSR-4
    standards.

--- app/Models/PrePostagem.php
+++ app/Models/PrePostagem.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 
-class Prepostagem extends Model implements Auditable
+class PrePostagem extends Model implements Auditable
 {
   use \OwenIt\Auditing\Auditable;
 
